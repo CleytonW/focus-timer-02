30b1bd5d8630f07d73e2379fb2eb62966f453598
Cleyton Melo <cleytonw11@gmail.com>
2022-09-24 13:17:14 -0300

    added play and stop function

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const buttonPlay = document.querySelector('.play');
+const buttonStop = document.querySelector('.stop')
+const buttonAddMinutes = document.querySelector('.increase')
+const buttonRemoveMinutes = document.querySelector('.decrease')
+
+
+buttonPlay.addEventListener('click', () => {
+  buttonPlay.classList.add('hide')
+})
+
+buttonStop.addEventListener('click', () => {
+  buttonPlay.classList.remove('hide')
+})
+
+buttonAddMinutes.addEventListener('click', () => {
+  buttonAddMinutes.classList.add
+})
