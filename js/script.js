5f574a208526ecc34a2f56cba64b842077327dd2
Cleyton Melo <cleytonw11@gmail.com>
2022-11-13 22:56:56 -0300

    finished function

--- js/script.js
+++ js/script.js
@@ -14,12 +14,37 @@ const secondsDisplay = document.querySelector('.seconds');
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
 
+// ----- AUDIO -----
+const forestAudio = new Audio('/assets/sounds/Floresta.wav')
+const rainingAudio = new Audio('/assets/sounds/Chuva.wav')
+const coffeeShopAudio = new Audio('/assets/sounds/Cafeteria.wav')
+const firePlaceAudio = new Audio('/assets/sounds/Lareira.wav')
+const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+
+forestAudio.loop = true
+rainingAudio.loop = true
+coffeeShopAudio.loop = true
+firePlaceAudio.loop = true
 
 
-// const forestAudio = new Audio('/sounds/Floresta.wav')
-// const rainingAudio = new Audio('/sounds/Chuva.wav')
-// const coffeeShopAudio = new Audio('/sounds/Cafeteria.wav')
-// const firePlaceAudio = new Audio('/sounds/Lareira.wav')
+// ----- FUNÇÕES -----
+let actualAudio
+function audioPlaying(audio) {
+  const audioPlaying = actualAudio === audio
+
+  if (audioPlaying) {
+    audio.pause()
+    actualAudio = false
+    return true
+  }
+
+  return false
+}
+
+function pressButton() {
+  buttonPressAudio.play()
+}
 
 function resetControls() {
   buttonPlay.classList.remove('hide');
@@ -36,20 +61,39 @@ function updateTimerDisplay(minutes, seconds) {
   secondsDisplay.textContent = String(seconds).padStart(2, "0");
 }
 
+function addMinutes() {
+  let minutes = Number(minutesDisplay.textContent)
+  let seconds = Number(secondsDisplay.textContent)
+
+  updateTimerDisplay(String(minutes + 5), seconds)
+}
+
+function removeMinutes() {
+  let minutes = Number(minutesDisplay.textContent)
+  let seconds = Number(secondsDisplay.textContent)
+  if (minutes <= 4) {
+    return
+  } else {
+    updateTimerDisplay(String(minutes - 5), seconds)
+  }
+}
+
 function countdown () {
   timerTimeOut = setTimeout(() => {
     let seconds = Number(secondsDisplay.textContent);
     let minutes = Number(minutesDisplay.textContent);
+    let isFinished = minutes <= 0 && seconds <= 0
     
     updateTimerDisplay(minutes, 0)
 
-    if (minutes <= 0) {
+    if (isFinished) {
       resetControls()
+      kitchenTimer.play()
       return
     }
     
     if (seconds <= 0) {
-      seconds = 2;
+      seconds = 60;
       --minutes
     };
     
@@ -59,28 +103,42 @@ function countdown () {
   }, 1000);
 };
 
+// ----- BUTTÕES -----
+
 buttonPlay.addEventListener('click', () => {
   buttonPlay.classList.add('hide');
   buttonPause.classList.remove('hide');
+
   countdown();
+  pressButton()
 })
 
 buttonPause.addEventListener('click', () => {
   buttonPlay.classList.remove('hide');
   buttonPause.classList.add('hide');
+
   clearTimeout(timerTimeOut);
   resetControls();
+  pressButton()
 })
 
 buttonStop.addEventListener('click', () => {
   buttonPlay.classList.remove('hide');
   buttonPause.classList.add('hide');
+
   resetControls()
   resetTimer()
+  pressButton()
 })
 
 buttonAddMinutes.addEventListener('click', () => {
-  buttonAddMinutes.classList.add;
+  addMinutes()
+  pressButton()
+})
+
+buttonRemoveMinutes.addEventListener('click', () => {
+  removeMinutes()
+  pressButton()
 })
 
 buttonForest.addEventListener('click', function () {
@@ -88,6 +146,16 @@ buttonForest.addEventListener('click', function () {
   buttonRain.classList.remove('active')
   buttonCoffeeShop.classList.remove('active')
   buttonFirePlace.classList.remove('active')
+
+  if (audioPlaying(forestAudio)) {
+    return
+  }
+  if (actualAudio) {
+    actualAudio.pause()
+  }
+
+  forestAudio.play()
+  actualAudio = forestAudio
 })
 
 buttonRain.addEventListener('click', function () {
@@ -95,6 +163,16 @@ buttonRain.addEventListener('click', function () {
   buttonForest.classList.remove('active')
   buttonCoffeeShop.classList.remove('active')
   buttonFirePlace.classList.remove('active')
+
+  if (audioPlaying(rainingAudio)) {
+    return
+  }
+  if (actualAudio) {
+    actualAudio.pause()
+  }
+
+  rainingAudio.play()
+  actualAudio = rainingAudio
 })
 
 buttonCoffeeShop.addEventListener('click', function () {
@@ -102,6 +180,16 @@ buttonCoffeeShop.addEventListener('click', function () {
   buttonRain.classList.remove('active')
   buttonForest.classList.remove('active')
   buttonFirePlace.classList.remove('active')
+
+  if (audioPlaying(coffeeShopAudio)) {
+    return
+  }
+  if (actualAudio) {
+    actualAudio.pause()
+  }
+
+  coffeeShopAudio.play()
+  actualAudio = coffeeShopAudio
 })
 
 buttonFirePlace.addEventListener('click', function () {
@@ -109,4 +197,14 @@ buttonFirePlace.addEventListener('click', function () {
   buttonRain.classList.remove('active')
   buttonCoffeeShop.classList.remove('active')
   buttonForest.classList.remove('active')
+
+  if (audioPlaying(firePlaceAudio)) {
+    return
+  }
+  if (actualAudio) {
+    actualAudio.pause()
+  }
+
+  firePlaceAudio.play()
+  actualAudio = firePlaceAudio
 })
